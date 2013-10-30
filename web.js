1115918aa9c51a31b4c59d6ef6e55b71c20f410e
reducedtobinary <qinglan.zhou@gmail.com>
2013-10-30 15:27:15 +0000

    Updated web.js

--- web.js
+++ web.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var app = express();
+var fs = require("fs");
+var data = fs.readFileSync("index.html", "utf8");
 app.use(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(data);
 });
 
 var port = process.env.PORT || 5000;
